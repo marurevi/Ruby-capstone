4d0722cf8eecd0ce8e0523d823df5b0ba0bf7dd2
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-01 12:24:45 -0500

    Add id with test

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,9 @@
+require 'securerandom'
+
 class Item
+  attr_reader :id
+  
+  def initialize(id = SecureRandom.uuid)
+    @id = id
+  end
 end
--- spec/unit/item_spec.rb
+++ spec/unit/item_spec.rb
@@ -1,14 +1,14 @@
-# Your spec stuff goes here...
 require_relative '../../lib/item'
 
 describe Item do
   before(:all) do 
-    @item = Item.new
+    @item = Item.new('the-item-id')
   end
   
   context 'When testing the item class' do
     it 'should instantiate the item class with the properties' do
       expect(@item).to be_truthy
+      expect(@item.id).to eq 'the-item-id'
     end
   end
 end
