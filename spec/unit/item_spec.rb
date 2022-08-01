b9552165a2549e058a5001b75baabecd26ee7d12
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-01 11:57:31 -0500

    Add initialize item test

---
+++ spec/unit/item_spec.rb
@@ -0,0 +1,15 @@
+# Your spec stuff goes here...
+require_relative '../../lib/item'
+
+describe Item do
+  before(:all) do 
+    item = Item.new
+  end
+
+  context 'When testing the item class' do
+    it 'should instantiate the item class with the properties' do
+    #   app = App.new
+    #   expect(app.status).to eq 'Created'
+    end
+  end
+end
