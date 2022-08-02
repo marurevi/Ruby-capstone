4d2a77d4f1be7bd5e702e925bbe12f4908f25db7
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-02 15:36:19 -0500

    Fix tests

--- spec/unit/author_spec.rb
+++ spec/unit/author_spec.rb
@@ -4,10 +4,10 @@ fdescribe 'An author' do
   let(:author) { Author.new('first-name', 'last-name', 'the-author-id') }
 
   it 'should be initialized as an author object with properties' do
-    expect(author.is_a?(Author)).to be true
+    expect(author).to be_a Author
     expect(author.first_name).to eq 'first-name'
     expect(author.last_name).to eq 'last-name'
-    expect(author.author_id).to eq 'the-author-id'
+    expect(author.id).to eq 'the-author-id'
     expect(author.items.size).to eq 0
   end
 
