39b4c1c0bb1ebff3e5203d43a3fc9acc0dc50443
marurevi <maryrev@hotmail.com>
2022-08-02 09:11:49 -0300

    Update Item class

--- spec/unit/book_spec.rb
+++ spec/unit/book_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/item'
-fdescribe "A book" do
-it "should be an instance of the item" do
-  expect(@object.should be_an_instance_of Item)
-end
-it "should overwrite parent method if cover_state is bad"
+fdescribe 'A book' do
+  it 'should be an instance of the item' do
+    expect(@object.should(be_an_instance_of(Item)))
+  end
+  it 'should overwrite parent method if cover_state is bad'
 end
