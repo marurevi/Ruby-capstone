3ded1cc2b0fd48c66f1ad772dc1547c41b74c409
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-02 04:12:39 -0600

    Added method for music album and test file for music album

--- lib/musicalbum.rb
+++ lib/musicalbum.rb
@@ -1,3 +1,34 @@
-class MusicAlbum 
-    
-end
+require_relative 'item'
+
+# class MusicAlbum < Item
+# #   attr_accessor :on_spotify
+
+# #   def initialize(published_date, on_spotify: true)
+# #     super(published_date, archived: archived)
+# #     @on_spotify = on_spotify
+# #   end
+#   attr_accessor :on_spotify, :name, :genre, :publish_date
+
+#   def initialize(name, genre, publish_date, on_spotify)
+#     @name = name
+#     @genre = genre
+#     super(publish_date)
+#     @on_spotify = on_spotify
+#   end
+
+#   def can_be_archived?
+#     super && @on_spotify
+#   end
+# end
+class MusicAlbum < Item
+    attr_accessor :on_spotify
+  
+    def initialize(published_date, on_spotify: true, archived: false)
+      super(published_date, archived: archived)
+      @on_spotify = on_spotify
+    end
+  
+    def can_be_archived?
+      super && @on_spotify
+    end
+  end
