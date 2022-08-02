f63825cae56aac3dd50f24a9096e9fa0b5673982
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-02 04:23:11 -0600

    Added Method for genre

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,13 @@
 class Genre
+    attr_accessor :items
+    def initialize(name)
+        @name = name
+        @id = rand 1..1000
+        @items = []
+    end
 
+    def add_item(item)
+        @items.push(item) unless @items.include?(item)
+        item.add_genre(self)
+    end
 end
