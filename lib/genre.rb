88fdd453fefc2a804119d94bfa5bc034ecbd2b9a
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-08-03 10:42:49 -0600

    Added json method for genre

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,4 +12,21 @@ class Genre
     item.genre = self if item.class != String
     @items << item
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'name' => @name,
+      'id' => @id,
+      'items' => @items.map(&:id)
+    }.to_json(*args)
+  end
+
+  def self.json_create(genre)
+    newgenre = new(
+      genre['name'], genre['id']
+    )
+    genre['items'].each { |item| newgenre.add_item(item) }
+    newgenre
+  end
 end
